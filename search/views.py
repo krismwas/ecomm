636f72dd0bf20eef22c9cdcacf7018f1c285dc46
anonymous <krismwasgit@gmail.com>
2021-06-15 15:05:05 +0300

    featured products

--- search/views.py
+++ search/views.py
@@ -11,15 +11,35 @@ class ProductSearchView(ListView):
     # model = Product
     template_name = 'search/view.html'
 
+    # def get_context_data(self, **kwargs):
+    #     # query = self.request.GET.get('q')
+    #     context = super().get_context_data(**kwargs)
+    #     context['query'] = self.request.GET.get('q')
+    #     print("gggggggggggggggggggggg")
+    #     print(self.request.GET.get('q'))
+    #     return context
+
     def get_queryset(self, *args, **kwargs):
-        print(self.request.GET)
         query = self.request.GET.get('q')
-        print("kkkkkkkkkkkkkkkkk")
-        print(query)
-        if query is not None:
-            return self.queryset.filter(title__icontains=query)
-            # return Product.objects.filter(title__icontains=query)
+        if query:
+            p = self.queryset.filter(title__icontains=query)
+            if p.exists():
+                return p
         return Product.objects.featured()
+
+        # print(self.request.GET)
+        # print('nnnnnn')
+        # print(query)
+        # print('nnnnnnn')
+        # if query is not None:
+        #     p = self.queryset.filter(title__icontains=query)
+        #     print(p)
+        #     print(p.exists())
+        #     if p.exists():
+        #         print("anita")
+        #         return p
+        # print("kamau")
+        # return Product.objects.featured()
 
 
 
