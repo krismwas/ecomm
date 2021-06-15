7552fb152b3a0d71e83efc468824f72ce023749d
anonymous <krismwasgit@gmail.com>
2021-06-15 11:18:25 +0300

    renamed url to query

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import ProductSearchView
 
 urlpatterns = [
-    url(r'^$', ProductSearchView.as_view(), name='search_component'),
+    url(r'^$', ProductSearchView.as_view(), name='query'),
 
 ]
