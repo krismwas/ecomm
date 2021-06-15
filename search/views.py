d123c3a5975348eda129542fb0ee76242d26b1cf
anonymous <krismwasgit@gmail.com>
2021-06-15 18:12:01 +0300

    Q search

--- search/views.py
+++ search/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.db.models import Q
 from django.views.generic.list import ListView
 
 from products.models import Product
@@ -8,38 +8,18 @@ from products.models import Product
 
 class ProductSearchView(ListView):
     queryset = Product.objects.all()
-    # model = Product
     template_name = 'search/view.html'
-
-    # def get_context_data(self, **kwargs):
-    #     # query = self.request.GET.get('q')
-    #     context = super().get_context_data(**kwargs)
-    #     context['query'] = self.request.GET.get('q')
-    #     print("gggggggggggggggggggggg")
-    #     print(self.request.GET.get('q'))
-    #     return context
 
     def get_queryset(self, *args, **kwargs):
         query = self.request.GET.get('q')
         if query:
-            p = self.queryset.filter(title__icontains=query)
+            looks_ups = Q(title__icontains=query) | Q(description__icontains=query)
+            p = self.queryset.filter(looks_ups)
             if p.exists():
                 return p
         return Product.objects.featured()
 
-        # print(self.request.GET)
-        # print('nnnnnn')
-        # print(query)
-        # print('nnnnnnn')
-        # if query is not None:
-        #     p = self.queryset.filter(title__icontains=query)
-        #     print(p)
-        #     print(p.exists())
-        #     if p.exists():
-        #         print("anita")
-        #         return p
-        # print("kamau")
-        # return Product.objects.featured()
+
 
 
 
