0ea78d2ef6d600067c7cff8c07c0039ced551593
anonymous <krismwasgit@gmail.com>
2021-06-16 14:45:25 +0300

    associated session id with user object

--- carts/models.py
+++ carts/models.py
@@ -4,12 +4,19 @@ from django.conf import settings
 from products.models import Product
 
 
+class CartModelManager(models.Manager):
+    def new(self, user):
+        return self.model.objects.create(user=user)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    objects = CartModelManager()
 
     def __str__(self):
         return str(self.id)
--- carts/views.py
+++ carts/views.py
@@ -4,18 +4,16 @@ from .models import Cart
 
 
 def cart_home(request):
-    request.session['cart_id'] = '12'
     cart_id = request.session.get('cart_id', None)
-    # if cart_id is None:
-    #     cart_obj = Cart.objects.create(user=None)
-    #     request.session['cart_id'] = cart_obj.id
-
     qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
         print('cart_id exists')
         cart_obj = qs.first()
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        cart_obj = Cart.objects.create(user=None)
+        cart_obj = Cart.objects.new(user=request.user)
         request.session['cart_id'] = cart_obj.id
 
     context = {}
