0c842dad9b704faef19d1c3e6742afcc010a2230
anonymous <krismwasgit@gmail.com>
2021-06-16 12:31:32 +0300

    session id

--- carts/views.py
+++ carts/views.py
@@ -4,5 +4,12 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+    cart_id = request.session.get('cart_id', None)
+    if cart_id is None:
+        print('create new cart')
+        request.session['cart_id'] = 12
+    else:
+        print('cart_id exists')
+
     context = {}
     return render(request, 'carts/home.html', context)
