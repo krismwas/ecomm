1c0023d9dd29d2910736000f26484a236ae5787f
anonymous <krismwasgit@gmail.com>
2021-06-17 14:36:32 +0300

    signals update

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from carts.models import Cart
 from ecomm_home.utils import unique_order_id_generator
@@ -22,10 +22,41 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    def update_total(self):
+        cart_total = self.cart.total
+        shipping_total = self.shipping_total
+        new_total = cart_total + shipping_total
+        self.total = new_total
+        self.save()
+        return new_total
 
-def pre_save_order_receiver(instance, **kwargs):
+
+def post_save_order(instance, created, **kwargs):
+    if created:
+        instance.update_total()
+
+
+post_save.connect(post_save_order, sender=Order)
+
+
+def post_save_cart_total(instance, created, **kwargs):
+    print("ggggggggggggggggggggggggggggggggggggggggggggg")
+    if not created:
+        cart_obj = instance
+        qs = Order.objects.filter(cart__id=cart_obj.id)
+        if qs.count == 1:
+            print("adddddddddddddddddddddddddddddddddddddddddddddd")
+            order_obj = qs.first()
+            order_obj.update_total()
+
+
+post_save.connect(post_save_cart_total, sender=Cart)
+
+
+def pre_save_unique_order_id(instance, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
 
 
-pre_save.connect(pre_save_order_receiver, sender=Order)
+pre_save.connect(pre_save_unique_order_id, sender=Order)
+
