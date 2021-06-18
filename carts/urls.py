61165f062930151e43acb1e117ca82f1a2e8b8cf
anonymous <krismwasgit@gmail.com>
2021-06-18 12:36:17 +0300

    checkout url

--- carts/urls.py
+++ carts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import cart_home, cart_update
+from .views import cart_home, cart_update, checkout
 
 urlpatterns = [
     url(r'^$', cart_home, name='home'),
     url(r'^update/$', cart_update, name='update'),
+    url(r'^checkout/$', checkout, name='check_out'),
     # url(r'^product-detail/(?P<pk>\d+)/$', ProductDetail.as_view()),
 
 ]
