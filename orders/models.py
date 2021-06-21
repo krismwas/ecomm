f6fe2f030a669f835c88ec9c68016cb58ba7d5a8
anonymous <krismwasgit@gmail.com>
2021-06-21 13:04:11 +0300

    billing profile for order

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import pre_save, post_save
 
 from carts.models import Cart
+from billing.models import BillingProfile
 from ecomm_home.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
@@ -13,11 +14,13 @@ ORDER_STATUS_CHOICES = (
 
 
 class Order(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
     order_id = models.CharField(max_length=120, blank=True)
     cart = models.ForeignKey(Cart)
     status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=5.99, max_digits=50, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=50, decimal_places=2)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.order_id
