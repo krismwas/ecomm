7503b999be20cb37ec55479784f9ccd18329fc5b
anonymous <krismwasgit@gmail.com>
2021-06-22 17:14:46 +0300

    checkout reuse view

--- addresses/views.py
+++ addresses/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
+# CRUD create update retrieve delete
 
-from addresses.forms import AddressForm
 from billing.models import BillingProfile
+from .forms import AddressForm
+from .models import Adress
 
 
 def checkout_address_create_view(request):
     form = AddressForm(request.POST or None)
     context = {
-        'form': form
+        "form": form
     }
     next_ = request.GET.get('next')
     next_post = request.POST.get('next')
-    request_path = next_ or next_post or None
+    redirect_path = next_ or next_post or None
     if form.is_valid():
+        print(request.POST)
         instance = form.save(commit=False)
         billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
         if billing_profile is not None:
@@ -24,11 +27,29 @@ def checkout_address_create_view(request):
             request.session[address_type + "_address_id"] = instance.id
             print(address_type + "_address_id")
         else:
-            print('Error here')
-            return redirect("cart:check_out")
+            print("Error here")
+            return redirect("cart:checkout")
 
-        if is_safe_url(request_path, request.get_host()):
-            return redirect(request_path)
-        else:
-            return redirect('cart:check_out')
-    return redirect('cart:check_out')
+        if is_safe_url(redirect_path, request.get_host()):
+            return redirect(redirect_path)
+    return redirect("cart:checkout")
+
+
+def checkout_address_reuse_view(request):
+    if request.user.is_authenticated():
+        context = {}
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+        if request.method == "POST":
+            print(request.POST)
+            shipping_address = request.POST.get('shipping_address', None)
+            address_type = request.POST.get('address_type', 'shipping')
+            billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+            if shipping_address is not None:
+                qs = Adress.objects.filter(billing_profile=billing_profile, id=shipping_address)
+                if qs.exists():
+                    request.session[address_type + "_address_id"] = shipping_address
+                if is_safe_url(redirect_path, request.get_host()):
+                    return redirect(redirect_path)
+    return redirect("cart:checkout")
