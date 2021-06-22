6c772425f84cb06089c7bb94e3e02f114084b03a
anonymous <krismwasgit@gmail.com>
2021-06-22 10:59:18 +0300

    fixed sessions for checkout

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from products.models import Product
 from addresses.forms import AddressForm
 from orders.models import Order
+from addresses.models import Adress
 from accounts.forms import LoginForm, GuestForm
 from billing.models import BillingProfile
 from accounts.models import GuestEmail
@@ -39,11 +40,20 @@ def checkout_home(request):
     login_form = LoginForm()
     guest_form = GuestForm()
     address_form = AddressForm()
-    # billing_address_form = AddressForm()
+    billing_address_id = request.session.get('billing_address_id', None)
+    shipping_address_id = request.session.get('shipping_address_id', None)
 
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
     if billing_profile is not None:
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
+        if shipping_address_id:
+            order_obj.shipping_address = Adress.objects.get(id=shipping_address_id)
+            del request.session['shipping_address_id']
+        if billing_address_id:
+            order_obj.billing_address = Adress.objects.get(id=billing_address_id)
+            del request.session['billing_address_id ']
+        if billing_address_id or shipping_address_id:
+            order_obj.save()
 
     context = {
         "login_form": login_form,
