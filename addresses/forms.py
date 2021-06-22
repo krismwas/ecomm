51e508fe409de98f4e8b63579d721ff148c37bd4
anonymous <krismwasgit@gmail.com>
2021-06-22 09:55:42 +0300

    fixed address form

--- addresses/forms.py
+++ addresses/forms.py
@@ -6,3 +6,7 @@ from .models import Adress
 class AddressForm(forms.ModelForm):
     class Meta:
         model = Adress
+        fields = [
+            'address_line_1', 'address_line_2',
+            'city', 'country', 'state', 'postal_code'
+        ]
